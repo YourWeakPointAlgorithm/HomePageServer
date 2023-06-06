4e1931428d511c1ab7924e79273f2c9e12a975bc
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-06-07 02:04:13 +0900

    Init Index.js

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const cron = require("node-cron");
+const {Client} = require("@notionhq/client");
+require('dotenv').config();
+require('expose-gc');
+
+const notion = new Client({auth: process.env.NOTION_API_KEY});
+
+const todayProblemDatabaseId = "4fd55fa459684f4f82e6fc6684c2940b";
+
+cron.schedule('0 */5 * * * *', () => global.gc());
+
+router.get('/', function (req, res, next) {
+  res.send('hello world!')
+});
 
 module.exports = router;
